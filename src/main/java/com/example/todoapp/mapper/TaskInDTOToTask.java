6cc13dbcb73d59b004487e1bf17adfa42736a8e4
DanielaSanchezb <84026755+DanielaSanchezb@users.noreply.github.com>
2023-10-02 16:03:34 -0500

    first commit

---
+++ src/main/java/com/example/todoapp/mapper/TaskInDTOToTask.java
@@ -0,0 +1,25 @@
+package com.example.todoapp.mapper;
+
+import com.example.todoapp.persistence.entity.Task;
+import com.example.todoapp.persistence.entity.TaskStatus;
+import com.example.todoapp.service.dto.TaskInDTO;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class TaskInDTOToTask implements IMapper<TaskInDTO, Task>{
+
+    /*we convert the object that we ask the user through the DTO to a complete one*/
+    @Override
+    public Task map(TaskInDTO in) {
+        Task task = new Task();
+        task.setTitle(in.getTitle());
+        task.setDescription(in.getDescription());
+        task.setEta(in.getEta());
+        task.setCreatedDate(LocalDateTime.now());
+        task.setFinished(false);
+        task.setTaskStatus(TaskStatus.ON_TIME);
+        return task;
+    }
+}
---
+++ src/main/java/com/example/todoapp/persistence/entity/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.example.todoapp.persistence.entity;
+
+public enum TaskStatus {
+
+    ON_TIME, LATE
+}
